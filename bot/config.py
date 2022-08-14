ccbad5bc742373c9c0501a86242f7bbcf9826a3d
amitnara126 <101616176+amitnara126@users.noreply.github.com>
2022-08-14 16:18:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,13 +3,13 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("7774029"))
+    API_HASH = os.environ.get("531dbf42d387514dc43da07db9f2dc8f")
+    BOT_TOKEN = os.environ.get("5264031481:AAElVQQ19zSav2s1WyoFfOtFt7QXuhdrReA")
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+    LOG_CHANNEL = int(os.environ.get("VCAM_CHANNLE"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1654867043").split(" ")]
     MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
